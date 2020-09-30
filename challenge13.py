52097237188380c7cf11ec3ace9ee6bde02bd626
Utkarsh Singh <utkarsh_1901cs67@iitp.ac.in>
2020-09-30 23:55:37 +0530

    Create challenge13.py

---
+++ challenge13.py
@@ -0,0 +1,17 @@
+# challenge 13
+
+import pygame
+pygame.init()
+
+win = pygame.display.set_mode((1000, 1000))
+
+pygame.display.set_caption("challenge 13")
+
+run = True
+
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            run = False
+
+pygame.quit()s
