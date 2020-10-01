5151dd418e631c22f906dfc158d5d60e8de9db81
Utkarsh Singh <utkarsh_1901cs67@iitp.ac.in>
2020-10-02 01:36:48 +0530

    Create challenge14.py

---
+++ challenge14.py
@@ -0,0 +1,4 @@
+# challenge 14
+
+f1 = open("file.txt","a")   # write mode and stream is positioned at the end of the file
+f2 = open("file.txt","r")   # read mode and stream is positioned at the beginning of the file.
