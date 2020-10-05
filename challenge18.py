bcb471ed33fe18eca2bd7bde324361677c49b0f3
Utkarsh Singh <utkarsh_1901cs67@iitp.ac.in>
2020-10-06 01:39:22 +0530

    Create challenge18.py

---
+++ challenge18.py
@@ -0,0 +1,11 @@
+# challenge 18
+
+def fact(n):
+    if n == 1 or n == 0:
+        return 1
+    else:
+        return n*fact(n-1)
+
+
+n = int(input("Enter value of n : "))
+print("The factorial of n is ", fact(n))
