f776c3b3d8d9d6d5edeb96b30de44e0c24e79b43
Utkarsh Singh <utkarsh_1901cs67@iitp.ac.in>
2020-10-13 16:32:21 +0530

    Update challenge23.py

--- challenge23.py
+++ challenge23.py
@@ -1,33 +1,35 @@
 # challenge 23
-# Function renameimages() takes a list of filenames in the directory and path of the directory, and renames .jpg files
-
 
 import os
 
 
 def renameimages(filelist, path):
 
-    count = 1
+    count =1
 
-    for file in filelist:
-        extension = file.split(".")[-1].lower()
+    for c,file in enumerate(os.listdir(path)):
+
+        if file in filelist:
+            continue
+        extension=file.split(".")[-1].lower()
         if extension != "jpg":
             continue
-        newfile = str(count) + file[0].upper() + file[1:]
-        
-        src = os.path.join(path,file)
-        dst = os.path.join(path,newfile)
 
-        os.rename(src,dst)
+        newfile=str(count) + file[0].upper() + file[1:]
 
-        count+=1
+        src=os.path.join(path, file)
+        dst=os.path.join(path, newfile)
+
+        os.rename(src, dst)
+
+        count += 1
 
 
 def main():
-    path = os.getcwd()
-    files= ["aaa (1).jpg","aaa .txt","aaa (2).jpg","aaa (3).jpg","aaa (4).jpg","aaa (5).jpg","aaa (6).jpg","aaa (7).jpg"]
+    path=os.getcwd()
+    files=["aaa (1).jpg", "aaa (2).jpg"]  # files not to be renamed
 
-    renameimages(files,path)
+    renameimages(files, path)
 
 if __name__ == "__main__":
     main()
